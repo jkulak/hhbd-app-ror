93f8cfbb89fc217a14b2a609d6e8dd5286a43d0f
Jakub.Kulak <Jakub.Kulak@c9e358a8-5b6b-11de-bc7c-c9df4fa8b3c6>
2009-06-30 23:50:45 +0000

    + dodalem plugin do autocomplete

--- app/models/album.rb
+++ app/models/album.rb
@@ -10,21 +10,14 @@ class Album < ActiveRecord::Base
   belongs_to :editor, :class_name => "User", :foreign_key => "updated_by"
                     
   def media_types
-    # {"CD" => 1, "LP" => 2, "EP" => 3, "MP3" => 4, "MC" => 5, "Inne? Ale jakie???" => 6}
     {1 => "CD", 2 => "LP", 3 => "EP", 4 => "MP3", 5 => "MC", 6 => "Inne? Ale jakie???"}
   end
   
+=begin
+  TODO zrobić z tego partiala
+=end
   def url_name
-  # callname = input.dup
-  #  callname.downcase!
-  #  callname.gsub!(/[Ąą]+/i,'a') # wielkie litery niepotrzebne, skoro mamy wyzej downcase
-  #  callname.gsub!(/[Ęę]+/i,'e') 
-  #  callname.gsub!(/[Óó]+/i,'o') 
-  #  callname.gsub!(/[Łł]+/i,'l') 
-  #  callname.gsub!(/[^a-z0-9]+/i, '-')
-  #  callname.gsub!(/(^[-]+|[-]+$)/, '')
-  #  return callname
-     self.name.gsub(/[^[:alnum:]]/,'-') # zamien wszysktkie znaki z name ktore nie sa alfanumeryczna na -
+     self.name.gsub(/[^[:alnum:]]/,'-')
   end
 
   def self.find_all_albums
@@ -38,11 +31,7 @@ class Album < ActiveRecord::Base
   def premiere
      self.released? ? release_date.strftime('%d %B %Y') : (to_be_released?) ? to_be_released : release_date.strftime('%d %B %Y')
    end
-  #  
-  #  def release_date?
-  #    !@release_date.nil?
-  #  end
-  
+
   def self.newest
     find(:all, :order => :created_at, :limit => 2)
   end
@@ -66,15 +55,12 @@ class Album < ActiveRecord::Base
   def self.group_albums(bands_ids)
     find(:all, :conditions => ['artist_id in(?)', bands_ids], :order => "release_date desc" )
   end
-  
+
   def to_param
     if self.artist.nil? 
       "#{id}-#{url_name}"
     else
       "#{id}-#{self.artist.url_name}-#{url_name}"
+    end
   end
-    
-  end
-  
-  
 end
